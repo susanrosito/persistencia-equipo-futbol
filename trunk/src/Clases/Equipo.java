876ea360ae784d02bae812f83dfb2ba2cb506cf7
tatiianamolinari@gmail.com <tatiianamolinari@gmail.com@15973f9e-a95f-257d-bfc0-2165690ae02f>
2011-08-18 13:20:35 +0000

    Aca envio nuevos cambios, incluyendo un package para que hagamos el Testing :)

--- trunk/src/Clases/Equipo.java
+++ trunk/src/Clases/Equipo.java
@@ -4,10 +4,15 @@ import Interfaces.Nombrable;
 
 public class Equipo implements  Nombrable {
 
+	
 	Tecnico tecnico;
 	private String nombre;
 	
-
+	Equipo (String nombre, Tecnico tec)
+	{
+		this.nombre=nombre;
+		this.tecnico=tec;
+	}
 
 	public Formacion armarFormacion(Equipo e) {
 	return this.tecnico.armarFormacion(e);
---
+++ trunk/src/Tests/EquipoTest.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import junit.framework.TestCase;
+
+public class EquipoTest extends TestCase {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
