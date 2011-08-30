9eafc40e6707d61ce0901c6df700a514db27c310
tatiianamolinari@gmail.com <tatiianamolinari@gmail.com@15973f9e-a95f-257d-bfc0-2165690ae02f>
2011-08-30 20:30:21 +0000

    Ahi subo cambios en la clase Equipo, y en su respectivo test.

--- trunk/src/test/java/Tests/TestEquipo.java
+++ trunk/src/test/java/Tests/TestEquipo.java
@@ -1,7 +1,11 @@
 package Tests;
 
 import Clases.Equipo;
+import Clases.Formacion;
+import Clases.Jugador;
 
+import java.util.ArrayList;
+import java.util.List;
 import Clases.Tecnico;
 import junit.framework.Assert;
 import junit.framework.TestCase;
@@ -12,12 +16,15 @@ public class TestEquipo extends TestCase {
   Equipo tstEquipo;
   String nombre;
   Tecnico tec;
+  List <Jugador> jug;
 	protected void setUp()
 	{
 		this.tec = createMock(Tecnico.class);
+		this.jug= new ArrayList<Jugador> ();
+		
 		
 		this.nombre= "Alfa";
-		this.tstEquipo= new Equipo(this.nombre, tec);
+		this.tstEquipo= new Equipo(this.nombre, tec, jug);
 	}
 	protected void tearDown()
 	{
@@ -28,12 +35,14 @@ public class TestEquipo extends TestCase {
 	{
 		Assert.assertEquals(this.nombre, this.tstEquipo.getNombre());
 		Assert.assertEquals(this.tec, this.tstEquipo.getTecnico());
+		Assert.assertEquals(this.jug, this.tstEquipo.getJugadores());
 	}
 
 	public void testArmarFormacion() {
-		expect(tec.armarFormacion(this.tstEquipo)).andReturn(null).once();
+		Formacion f = createMock(Formacion.class);
+		expect(tec.armarFormacion(this.tstEquipo)).andReturn(f).once();
 		replay(tec);
-		this.tstEquipo.armarFormacion();
+		Assert.assertEquals(f, this.tstEquipo.armarFormacion());
 		verify(tec);
 		
 	}
