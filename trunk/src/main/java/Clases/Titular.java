cbdd751f4c7a700398b6db5719aec97b60994564
tatiianamolinari@gmail.com <tatiianamolinari@gmail.com@15973f9e-a95f-257d-bfc0-2165690ae02f>
2011-09-20 20:44:10 +0000

    MRRRRRRRRRRRRRRRRRRRRRR (L

--- trunk/src/main/java/Clases/Titular.java
+++ trunk/src/main/java/Clases/Titular.java
@@ -2,26 +2,42 @@ package Clases;
 
 public class Titular {
 
-	private Jugador jugador;
-	private Posicion posicion;
-	
-	
-	public Titular(Posicion p, Jugador jug){
-		this.posicion = p;
-		this.jugador = jug;
-	}
-	public void setJugador(Jugador jugador) {
-		this.jugador = jugador;
-	}
-	public Jugador getJugador() {
-		return jugador;
-	}
-	public void setPosicion(Posicion posicion) {
-		this.posicion = posicion;
-	}
-	public Posicion getPosicion() {
-		return posicion;
-	}
-	
-	
+    private Jugador jugador;
+
+    private int id;
+
+    private Posicion posicion;
+
+    public Titular() {
+    }
+
+    public Titular(final Posicion p, final Jugador jug) {
+        this.posicion = p;
+        this.jugador = jug;
+    }
+
+    public void setJugador(final Jugador jugador) {
+        this.jugador = jugador;
+    }
+
+    public Jugador getJugador() {
+        return this.jugador;
+    }
+
+    public void setPosicion(final Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+    public Posicion getPosicion() {
+        return this.posicion;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
 }
