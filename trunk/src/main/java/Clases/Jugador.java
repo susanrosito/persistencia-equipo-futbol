8364c2671f8cd9af57266d95c6946cca91d89dda
tatiianamolinari@gmail.com <tatiianamolinari@gmail.com@15973f9e-a95f-257d-bfc0-2165690ae02f>
2011-09-20 19:29:20 +0000

    Manzana Rojaaaaaaaaaaaa :)

--- trunk/src/main/java/Clases/Jugador.java
+++ trunk/src/main/java/Clases/Jugador.java
@@ -1,56 +1,72 @@
 package Clases;
 
-
 import java.util.List;
-import java.util.Map;
 
 import Interfaces.Habilidad;
 import Interfaces.Nombrable;
 
 public class Jugador implements Nombrable, Habilidad {
-	private String nombre;
-	private List<HabilidadImp> habilidades;
+    private String nombre;
 
-	public Jugador(String nomb, List<HabilidadImp> habs){
-		this.nombre = nomb;
-		this.habilidades = habs;
-	}
-	
-	
-	public String getNombre() {
-		return this.nombre;
-	}
+    private int id;
 
-	
-	public int getValor(Posicion p) {
-		int valor = 0;
-		int maxVal = 0;
-		for(HabilidadImp hab: this.getHabilidades()){
-			valor = hab.getValor(p);
-			if(valor > maxVal){
-				maxVal = valor;
-			}
-		}
-		return maxVal;
-	}
-	
-	public void setHabilidad(HabilidadImp hab, int val){
-		//falta crear habilidad en caso de no encontrarla
-		for(HabilidadImp habAc:this.getHabilidades()){
-			if(habAc.getPosicion() == hab.getPosicion()){
-				habAc.setValor(hab.getValor());
-				break;
-			}
-		}
-	}
+    private List<HabilidadImp> habilidades;
 
-	public void setHabilidades(List<HabilidadImp> habilidades) {
-		this.habilidades = habilidades;
-	}
+    public Jugador() {
 
+    }
 
-	public List<HabilidadImp> getHabilidades() {
-		return habilidades;
-	}
+    public Jugador(final String nomb, final List<HabilidadImp> habs) {
+        this.nombre = nomb;
+        this.habilidades = habs;
+    }
+
+    @Override
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    @Override
+    public int getValor(final Posicion p) {
+        int valor = 0;
+        int maxVal = 0;
+        for (HabilidadImp hab : this.getHabilidades()) {
+            valor = hab.getValor(p);
+            if (valor > maxVal) {
+                maxVal = valor;
+            }
+        }
+        return maxVal;
+    }
+
+    public void setHabilidad(final HabilidadImp hab, final int val) {
+        // falta crear habilidad en caso de no encontrarla
+        for (HabilidadImp habAc : this.getHabilidades()) {
+            if (habAc.getPosicion() == hab.getPosicion()) {
+                habAc.setValor(hab.getValor());
+                break;
+            }
+        }
+    }
+
+    public void setHabilidades(final List<HabilidadImp> habilidades) {
+        this.habilidades = habilidades;
+    }
+
+    public List<HabilidadImp> getHabilidades() {
+        return this.habilidades;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public void setNombre(final String nombre) {
+        this.nombre = nombre;
+    }
 
 }
