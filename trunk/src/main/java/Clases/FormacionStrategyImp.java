4832e4753c45d38f4c41d4c15f0a21618785dac2
tatiianamolinari@gmail.com <tatiianamolinari@gmail.com@15973f9e-a95f-257d-bfc0-2165690ae02f>
2011-09-27 19:58:50 +0000

    MR

--- trunk/src/main/java/Clases/FormacionStrategyImp.java
+++ trunk/src/main/java/Clases/FormacionStrategyImp.java
@@ -14,6 +14,10 @@ public class FormacionStrategyImp implements FormacionStrategy {
 
     }
 
+    public FormacionStrategyImp() {
+
+    }
+
     @Override
     public Formacion armarFormacion(final Equipo e) {
 
@@ -26,35 +30,34 @@ public class FormacionStrategyImp implements FormacionStrategy {
 
     }
 
+    /**
+     * PRECONDICION: Encuentra por lo menos un jugador mayor a los demas. PROPOSITO: Busca al mejor jugador para la
+     * posicion que le pasan por parametro.
+     * 
+     * @param Jugador
+     *            jugadores
+     * @param P
+     *            �sicion pos
+     * 
+     * @return Titular
+     */
+    public Titular buscarMJugador(final ArrayList<Jugador> jugadores, final Posicion pos) {
 
-	/**
-	 * PRECONDICION: Encuentra por lo menos un jugador mayor a los demas.
-	 * PROPOSITO: Busca al mejor jugador para la posicion que le pasan por
-	 * parametro.
-	 * 
-	 * @param Jugador
-	 *            jugadores
-	 * @param P�sicion
-	 *            pos
-	 * 
-	 * @return Titular
-	 */
-	public Titular buscarMJugador(ArrayList<Jugador> jugadores, Posicion pos) {
-		
-		int valor;  
-		Jugador jugadorElegido = jugadores.get(0);
-		int mayor = jugadorElegido.getValor(pos);
-		
-		for (int i = 1; i < jugadores.size(); i++) {
-			Jugador jugador = jugadores.get(i);
-			valor = jugador.getValor(pos);
-			if (valor > mayor) {
-				mayor = valor;
-				jugadorElegido = jugador;
-			}
-		}
-		return new Titular(pos, jugadorElegido);
-	}
+        int valor;
+        Jugador jugadorElegido = jugadores.get(0);
+        int mayor = jugadorElegido.getValor(pos);
+
+        for (int i = 1; i < jugadores.size(); i++) {
+            Jugador jugador = jugadores.get(i);
+            valor = jugador.getValor(pos);
+            if (valor > mayor) {
+                mayor = valor;
+                jugadorElegido = jugador;
+            }
+        }
+        return new Titular(pos, jugadorElegido);
+    }
+
     /**
      * PRECONDICION: Encuentra por lo menos un jugador mayor a los demas. PROPOSITO: Busca al mejor jugador para la
      * posicion que le pasan por parametro.
