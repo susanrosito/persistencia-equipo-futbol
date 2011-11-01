c01f2a8260e249f8cc30c1d6c6e93a1ac06aa18b
tatianamolinari90@gmail.com <tatianamolinari90@gmail.com@15973f9e-a95f-257d-bfc0-2165690ae02f>
2011-11-01 20:22:22 +0000

    =)

--- src/main/java/homes/PartidoDeCopaHome.java
+++ src/main/java/homes/PartidoDeCopaHome.java
@@ -1,26 +1,29 @@
 package homes;
 
-import static org.hibernate.criterion.Restrictions.eq;
-import static org.hibernate.criterion.Restrictions.geProperty;
-
-import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.classic.Session;
 
 import commands.HibernateManager;
-
-import dominio.PartidoDeCopa;
 
 public class PartidoDeCopaHome {
 
     public int getCantPartidosDeCopaGanadosPor(final String nombre) {
         Session session = HibernateManager.instance().getSession();
-        Criteria criteria = session.createCriteria(PartidoDeCopa.class);
+        /*
+         * Criteria criteria = session.createCriteria(PartidoDeCopa.class); Criteria criteria1 =
+         * session.createCriteria(PartidoDeCopa.class); .groupProperty("title") Criteria criteriaEquipoLocal =
+         * criteria.createCriteria("ganadorP"); criteriaEquipoLocal.add(eq("nombre", nombre)) .addOrder(
+         * Order.desc("age") );
+         */
 
-        Criteria criteriaEquipoLocal = criteria.createCriteria("ganadorP");
-        criteriaEquipoLocal.add(eq("nombre", nombre));
+        Query query = session
+                .createQuery("select pc.ganadorP, count(*) as ganados from PartidoDeCopa as pc group by pc.ganadprP order by ganados asc");
 
-        criteria.add(geProperty("golesEquipoA", "golesEquipoB"));
-        return criteria.list().size();
+        return query.list().size();
+
+        /*
+         * criteria.add(geProperty("golesEquipoA", "golesEquipoB")); return criteria.list().size();
+         */
 
     }
 
