fe4d093cf0b1685354a4e3faa2e2848efa874b84
rosito.susana <rosito.susana@15973f9e-a95f-257d-bfc0-2165690ae02f>
2011-11-01 22:57:12 +0000

    LZSHVRIHBDFPEF

---
+++ src/test/java/dominio/ArmarFormacionTest.java
@@ -0,0 +1,13 @@
+package dominio;
+
+import commands.ArmarFormacionCommand;
+import commands.HibernateManager;
+import junit.framework.TestCase;
+
+public class ArmarFormacionTest extends TestCase{
+	
+	public void testApp() throws Exception {
+        HibernateManager.instance().runInSession(new ArmarFormacionCommand("Equipo C"));
+    }
+
+}
