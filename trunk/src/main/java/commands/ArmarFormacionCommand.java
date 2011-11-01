77314b828fe082b9b2517680ec14341679042018
rosito.susana <rosito.susana@15973f9e-a95f-257d-bfc0-2165690ae02f>
2011-11-01 22:56:36 +0000

    haWEUICGAWEIGYHVID

---
+++ trunk/src/main/java/commands/ArmarFormacionCommand.java
@@ -0,0 +1,30 @@
+package commands;
+
+import dominio.Equipo;
+import dominio.Formacion;
+import homes.EquipoHome;
+import interfaces.FormacionStrategy;
+
+public class ArmarFormacionCommand implements Runnable{
+	//Buscar un equipo por nombre, pedirle que arme una formación y persistir esa formación.
+
+    private final String nombreEquipo;
+
+    public ArmarFormacionCommand(final String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    @Override
+    public void run() {
+        Equipo e = new EquipoHome().getByName(this.nombreEquipo);
+        System.out.println(e.getNombre());
+        System.out.println(e.getTecnico());
+        System.out.println("CALABAZA " + e.getTecnico().getFormacionStrategy());
+        Formacion f = e.armarFormacion();
+        System.out.println(f);
+        System.out.println(e.getTecnico());
+       // e.getTecnico().setFormacionStrategy(f);
+    }
+
+
+}
